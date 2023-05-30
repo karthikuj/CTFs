a47de57903b6c76cffe16658cc54e3e068e1e0ea
karthikuj <karthikuj2001@gmail.com>
2023-05-30 19:37:48 +0530

    Makes some progress in Git Happens room.

---
+++ TryHackMe/git-happens/index-script-deobfuscated.js
@@ -0,0 +1,90 @@
+const eufemia = ["+( +[^", "471197", "value", "RegExp", "functi", "test", "CbRnH", "passwo", "userna", "TML", "tml", "a865c5", "+[^ ]}", "a5f298", "cookie", "admin", "3a71fd", "getEle", "login-", "^([^ ]", "TEhxP", "href", "f64cb3", "51a151", "d84319", "D USER", "digest", "R PASS", "oard.h", "error", " ]+)+)", "19a3c0", "f80f67", "/dashb", "bea070", "3ec9cb", "padSta", "from", "4004c2", "WORD!", "map", "NAME O", "encode", "INVALI", "a5106e", "baf89f", "6a7c7c", "elemen", "9a88db", "log", "join", "innerH", "SaltyB", "apply", "ned", "442a9d", "mentBy"];
+(function (barbi, kaylynn) {
+  const charmika = function (shawnice) {
+    while (--shawnice) {
+      barbi.push(barbi.shift());
+    }
+  }, jalna = function () {
+    const taleesha = {data: {key: "cookie", value: "timeout"}, setCookie: function (she, lourita, geneane, denario) {
+      denario = denario || {};
+      let alisa = lourita + "=" + geneane, avasophia = 0;
+      for (let navroop = 0, sherralyn = she.length; navroop < sherralyn; navroop++) {
+        const mica = she[navroop];
+        alisa += "; " + mica;
+        const epsie = she[mica];
+        she.push(epsie), sherralyn = she.length, epsie !== true && (alisa += "=" + epsie);
+      }
+      denario.cookie = alisa;
+    }, removeCookie: function () {
+      return "dev";
+    }, getCookie: function (allyn, avalon) {
+      allyn = allyn || function (cheyla) {
+        return cheyla;
+      };
+      const aakriti = allyn(new RegExp("(?:^|; )" + avalon.replace(/([.$?*|{}()[]\/+^])/g, "$1") + "=([^;]*)")), milferd = function (ranjan, kohyn) {
+        ranjan(++kohyn);
+      };
+      return milferd(charmika, kaylynn), aakriti ? decodeURIComponent(aakriti[1]) : undefined;
+    }}, kaitlan = function () {
+      const jemery = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+      return jemery.test(taleesha.removeCookie.toString());
+    };
+    taleesha.updateCookie = kaitlan;
+    let danesh = "";
+    const buck = taleesha.updateCookie();
+    if (!buck) taleesha.setCookie(["*"], "counter", 1); else buck ? danesh = taleesha.getCookie(null, "counter") : taleesha.removeCookie();
+  };
+  jalna();
+}(eufemia, 230));
+const allenah = function (eneri, kharizma) {
+  eneri = eneri - 0;
+  let duvall = eufemia[eneri];
+  return duvall;
+};
+const shantierra = function () {
+  let denaye = true;
+  return function (shahira, amirjon) {
+    if (allenah("0x4") === allenah("0x4")) {
+      const keyvin = denaye ? function () {
+        if (allenah("0x12") !== allenah("0x12")) {
+          function harumi() {
+            document[allenah("0xf") + allenah("0x36") + "Id"](allenah("0x1b"))["innerH" + allenah("0x7")] = allenah("0x29") + allenah("0x17") + "NAME O" + allenah("0x19") + allenah("0x25");
+          }
+        } else {
+          if (amirjon) {
+            const sier = amirjon[allenah("0x33")](shahira, arguments);
+            return amirjon = null, sier;
+          }
+        }
+      } : function () {};
+      return denaye = false, keyvin;
+    } else {
+      function shazil() {
+        const brittanya = denaye ? function () {
+          if (amirjon) {
+            const eulia = amirjon[allenah("0x33")](shahira, arguments);
+            return amirjon = null, eulia;
+          }
+        } : function () {};
+        return denaye = false, brittanya;
+      }
+    }
+  };
+}(), shylia = shantierra(this, function () {
+  const ativa = typeof window !== "undefi" + allenah("0x34") ? window : typeof process === "object" && typeof require === allenah("0x2") + "on" && typeof global === "object" ? global : this, syian = function () {
+    const minnie = new ativa[allenah("0x1")](allenah("0x11") + allenah("0x37") + allenah("0x1c") + allenah("0xa"));
+    return !minnie[allenah("0x3")](shylia);
+  };
+  return syian();
+});
+shylia();
+async function login() {
+  let lorria = document[allenah("0xf") + allenah("0x36") + "Id"](allenah("0x10") + "form");
+  console[allenah("0x2f")](lorria[allenah("0x2d") + "ts"]);
+  let mihaela = lorria[allenah("0x2d") + "ts"][allenah("0x6") + "me"][allenah("0x0")], naylene = await digest(lorria[allenah("0x2d") + "ts"][allenah("0x5") + "rd"][allenah("0x0")]);
+  mihaela === allenah("0xd") && naylene === allenah("0x24") + allenah("0xe") + "6ba9b0" + allenah("0x21") + "7eed08" + allenah("0x38") + allenah("0x16") + allenah("0x9") + allenah("0x35") + allenah("0x2c") + allenah("0x20") + "f3cb6a" + allenah("0x2a") + allenah("0x1e") + allenah("0x2e") + allenah("0x2b") + allenah("0x14") + allenah("0x15") + allenah("0xb") + allenah("0x1d") + "94eceb" + "bb" ? (document[allenah("0xc")] = "login=1", window.location[allenah("0x13")] = allenah("0x1f") + allenah("0x1a") + allenah("0x8")) : document["getEle" + allenah("0x36") + "Id"](allenah("0x1b"))[allenah("0x31") + allenah("0x7")] = allenah("0x29") + allenah("0x17") + allenah("0x27") + allenah("0x19") + allenah("0x25");
+}
+async function digest(eulina) {
+  const lebern = new TextEncoder, vincent = lebern[allenah("0x28")](eulina + (allenah("0x32") + "ob")), abdulazeem = await crypto.subtle[allenah("0x18")]("SHA-512", vincent), beneva = Array[allenah("0x23")](new Uint8Array(abdulazeem)), fort = beneva[allenah("0x26")](chiemi => chiemi.toString(16)[allenah("0x22") + "rt"](2, "0"))[allenah("0x30")]("");
+  return fort;
+}
--- TryHackMe/git-happens/solution.md
+++ TryHackMe/git-happens/solution.md
@@ -5,5 +5,12 @@
 
 ## Steps
 
+### Enumerate:
+1. Run a full NMAP port scan: `sudo nmap -v -sS -p- 10.10.48.67 | tee nmap-scan.txt`.
+2. A website is running on port 80, fuzz the directories/files: `sudo ffuf -u http://10.10.48.67/FUZZ -w /usr/share/wordlists/SecLists/Discovery/Web-Content/directory-list-lowercase-2.3-medium.txt -mc 200,301,302,403 -fs 6890 -v | tee ffuf-dir-fuzz.txt`.
+3. `/robots.txt` and `/sitemap.xml` did not reveal anything.
+4. The index page's source code has some obfuscated JS code, let's [deobfuscate](https://deobfuscate.io/) that.
+5. Saved it in the file `index-script-deobfuscated.js`.
+
 ### Task 1:
 #### Find the Super Secret Password
